f5c12415b4409dacb2732e539938840bed9ddb29
fandilladp <fandilladp@gmail.com>
2021-07-17 04:31:28 +0700

    create UI #stage 1 example

---
+++ client/bantucarioksigen/src/App.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Navbar } from "react-bootstrap";
+import Home from "./Home/Home";
+
+function App() {
+  return (
+    <div>
+      <Navbar
+        className="navbar fixed-top navbar-light"
+        style={{
+          backgroundColor: "#FFFFFF",
+          borderBottom: "1px solid #F1F3F0",
+        }}
+      >
+        <a className="navbar-brand" href="#">
+          <span
+            className="badge badge-danger"
+            style={{ fontSize: 25, backgroundColor: "#073180" }}
+          >
+            Bantu Cari Oksigen
+          </span>
+        </a>
+      </Navbar>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/bantucarioksigen/src/Home/Home.jsx
@@ -0,0 +1,118 @@
+import React, { select } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Container, Button, Row, Form, Col } from "react-bootstrap";
+import Card from "../component/Card";
+export default class Home extends React.Component {
+  render() {
+    return (
+      <div>
+        <Container
+          fluid
+          style={{
+            backgroundColor: "#F1F3F0",
+            paddingTop: 70,
+            paddingBottom: 30,
+          }}
+        >
+          <Form action method="get">
+            <Row className="justify-content-md-center">
+              <Col xs="auto">
+                <div>
+                  <Form.Label>Provinsi</Form.Label>
+                </div>
+                <div>
+                  <select
+                    className="selectpicker"
+                    data-style="select-new"
+                    data-live-search="true"
+                    data-size={5}
+                    id="propinsi"
+                    name="propinsi"
+                    data-width="100%"
+                  >
+                    <option value={0}>PILIH </option>
+                    <option value="11prop">ACEH </option>
+                    <option value="12prop">SUMATERA UTARA </option>
+                    <option value="13prop">SUMATERA BARAT </option>
+                    <option value="14prop">R I A U </option>
+                    <option value="15prop">JAMBI </option>
+                    <option value="16prop">SUMATERA SELATAN </option>
+                    <option value="17prop">BENGKULU </option>
+                    <option value="18prop" selected>
+                      LAMPUNG{" "}
+                    </option>
+                    <option value="19prop">KEPULAUAN BANGKA BELITUNG </option>
+                    <option value="20prop">KEPULAUAN RIAU </option>
+                    <option value="31prop">DKI JAKARTA </option>
+                    <option value="32prop">JAWA BARAT </option>
+                    <option value="33prop">JAWA TENGAH </option>
+                    <option value="34prop">D I YOGYAKARTA </option>
+                    <option value="35prop">JAWA TIMUR </option>
+                    <option value="36prop">BANTEN </option>
+                    <option value="51prop">BALI </option>
+                    <option value="52prop">NUSA TENGGARA BARAT </option>
+                    <option value="53prop">NUSA TENGGARA TIMUR </option>
+                    <option value="61prop">KALIMANTAN BARAT </option>
+                    <option value="62prop">KALIMANTAN TENGAH </option>
+                    <option value="63prop">KALIMANTAN SELATAN </option>
+                    <option value="64prop">KALIMANTAN TIMUR </option>
+                    <option value="65prop">KALIMANTAN UTARA </option>
+                    <option value="71prop">SULAWESI UTARA </option>
+                    <option value="72prop">SULAWESI TENGAH </option>
+                    <option value="73prop">SULAWESI SELATAN </option>
+                    <option value="74prop">SULAWESI TENGGARA </option>
+                    <option value="75prop">GORONTALO </option>
+                    <option value="76prop">SULAWESI BARAT </option>
+                    <option value="81prop">MALUKU </option>
+                    <option value="82prop">MALUKU UTARA </option>
+                    <option value="91prop">PAPUA BARAT </option>
+                    <option value="92prop">PAPUA </option>
+                  </select>
+                </div>
+              </Col>
+              <Col xs="auto">
+                <div>
+                  <Form.Label>Kab.Kota</Form.Label>
+                </div>
+                <div>
+                  <select
+                    className="selectpicker"
+                    data-style="select-new"
+                    data-live-search="true"
+                    data-size={3}
+                    id="kabkota"
+                    name="kabkota"
+                    data-width="100%"
+                  >
+                    <option value>Bandar Lampung</option>
+                  </select>
+                </div>
+              </Col>
+              <Col xs="auto">
+                <div className="my-4">
+                  <Button
+                    type="submit"
+                    style={{
+                      backgroundColor: "#073180",
+                      color: "#FFFFFF",
+                      padding: "6px 40px 6px 40px",
+                    }}
+                  >
+                    Cari
+                  </Button>
+                </div>
+              </Col>
+            </Row>
+          </Form>
+        </Container>
+        <br />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
+  }
+}
