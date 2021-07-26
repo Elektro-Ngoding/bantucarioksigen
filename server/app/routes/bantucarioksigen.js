c2853afa18acb25404dbd04602953ddfa65e28fe
fandilladp <fandilladp@gmail.com>
2021-07-26 14:20:22 +0700

    update crud dataOksigen #succes

--- server/app/routes/bantucarioksigen.js
+++ server/app/routes/bantucarioksigen.js
@@ -46,5 +46,60 @@ router.get("/", async (req, res) => {
   }
 });
 
+router.put('/:id', async(req, res) => {
+    try {
+        
+        const dataOksigenUpdate = await OksigenModel.updateOne({
+            namaToko: req.body.namaToko,
+            status: req.body.status,
+            auth: {
+              username: req.body.auth.username,
+              password: req.body.auth.password,
+            },
+            data: {
+              provinsi: req.body.data.provinsi,
+              kota: req.body.data.kota,
+              alamat: req.body.data.alamat,
+              kontak: req.body.data.kontak,
+              statusBuka: req.body.data.statusBuka,
+              stokBarang: req.body.data.stokBarang,
+              antrian: req.body.data.antrian,
+              waktuBuka: req.body.data.waktuBuka,
+            },
+        }) 
+        res.json(dataOksigenUpdate)
+
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const dataOksigenDelete = await OksigenModel.deleteOne({
+            _id:req.params.id
+        })
+        res.json("delete succes")
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+})
+
+router.get('/:id', async(req, res) => {
+    try {
+        const dataOksigenDetail = await OksigenModel.findById({
+            _id:req.params.id
+        })
+        res.json(dataOksigenDetail)
+    } catch (error) {
+        res.json({
+            message: error
+        })
+    }
+})
 
 module.exports = router;
