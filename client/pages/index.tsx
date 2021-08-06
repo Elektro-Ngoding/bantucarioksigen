417002cbf48f4dbfa43af378de32993888f77a7f
fandilladp <fandilladp@gmail.com>
2021-08-07 01:18:26 +0700

    add addres handler to homepage

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,4 +1,5 @@
 import { MDBCol, MDBContainer, MDBRow } from "mdbreact";
+import Address from "../component/Address";
 
 import Card from "../component/Card";
 import Layout from "../component/Layout/Layout";
@@ -24,77 +25,10 @@ export default function Home(props: DataProps) {
           <form action="" method="get">
             <MDBRow center>
               <MDBCol size="auto">
-                <div>
-                  <label>Provinsi</label>
-                </div>
-                <div>
-                  <select
-                    className="browser-default custom-select"
-                    data-style="select-new"
-                    data-live-search="true"
-                    data-size={5}
-                    id="propinsi"
-                    name="propinsi"
-                    data-width="100%"
-                  >
-                    <option value={0}>PILIH </option>
-                    <option>ACEH </option>
-                    <option>SUMATERA UTARA </option>
-                    <option>SUMATERA BARAT </option>
-                    <option>R I A U </option>
-                    <option>JAMBI </option>
-                    <option>SUMATERA SELATAN </option>
-                    <option>BENGKULU </option>
-                    <option selected>LAMPUNG</option>
-                    <option>KEPULAUAN BANGKA BELITUNG </option>
-                    <option>KEPULAUAN RIAU </option>
-                    <option>DKI JAKARTA </option>
-                    <option>JAWA BARAT </option>
-                    <option>JAWA TENGAH </option>
-                    <option>D I YOGYAKARTA </option>
-                    <option>JAWA TIMUR </option>
-                    <option>BANTEN </option>
-                    <option>BALI </option>
-                    <option>NUSA TENGGARA BARAT </option>
-                    <option>NUSA TENGGARA TIMUR </option>
-                    <option>KALIMANTAN BARAT </option>
-                    <option>KALIMANTAN TENGAH </option>
-                    <option>KALIMANTAN SELATAN </option>
-                    <option>KALIMANTAN TIMUR </option>
-                    <option>KALIMANTAN UTARA </option>
-                    <option>SULAWESI UTARA </option>
-                    <option>SULAWESI TENGAH </option>
-                    <option>SULAWESI SELATAN </option>
-                    <option>SULAWESI TENGGARA </option>
-                    <option>GORONTALO </option>
-                    <option>SULAWESI BARAT </option>
-                    <option>MALUKU </option>
-                    <option>MALUKU UTARA </option>
-                    <option>PAPUA BARAT </option>
-                    <option>PAPUA </option>
-                  </select>
-                </div>
+               <Address/>
               </MDBCol>
               <MDBCol size="auto">
-                <div>
-                  <label>Kab.Kota</label>
-                </div>
-                <div>
-                  <select
-                    className="browser-default custom-select"
-                    data-style="select-new"
-                    data-live-search="true"
-                    data-size={3}
-                    id="kabkota"
-                    name="kabkota"
-                    data-width="100%"
-                  >
-                    <option>Bandar Lampung</option>
-                  </select>
-                </div>
-              </MDBCol>
-              <MDBCol size="auto">
-                <div className="my-4">
+                <div className="mt-4">
                   <div
                     className="btn ml-1"
                     style={{
