6b74ce2b490917c9fd8559ce3c809f284ac81958
fandilladp <fandilladp@gmail.com>
2021-08-09 19:42:05 +0700

    admin handler delete mitra done

--- client/component/Table/index.tsx
+++ client/component/Table/index.tsx
@@ -1,11 +1,26 @@
 import { MDBDataTable } from "mdbreact";
-
+import { useState } from "react";
+import { deleteMitra } from "../../lib/deleteMitra";
+import Modal from "../Modal/index";
 interface DataTableProps {
-dataTab: Array<any>
+  dataTab: Array<any>;
+}
+interface Type {
+  id: string;
+  namaToko: string;
+  onClick: () => void;
 }
 
-
 const Table = (props: DataTableProps) => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handlerUpdate = () => {
+    setShowModal(true);
+  };
+  const handlerDelete = (id: Type, namaToko: Type) => {
+    alert("you will delete partner " + namaToko);
+    deleteMitra(id);
+  };
   const { dataTab } = props;
   const data = {
     columns: [
@@ -19,7 +34,7 @@ const Table = (props: DataTableProps) => {
         label: "Status",
         field: "status",
         sort: "asc",
-        width: 100
+        width: 100,
       },
       {
         label: "Provinsi",
@@ -64,36 +79,39 @@ const Table = (props: DataTableProps) => {
         width: 100,
       },
     ],
-    rows : dataTab.map(data => {
+    rows: dataTab.map((data) => {
       return {
-          namaToko: data.namaToko,
-
-          status: data.status,
-          provinsi: data.data.provinsi,
-          kota: data.data.kota,
-          alamat: data.data.alamat,
-          kontak: data.data.kontak,
-          updated_date: data.data.updated_date,
-          update: (
-            <a href="#">
-              <div className="btn btn-success">Update</div>
-            </a>
-          ),
-          delete: (
-            <a href="#">
-              <div className="btn btn-danger">Delete</div>
-            </a>
-          ),
-      }
-    })
+        namaToko: data.namaToko,
+        status: data.status,
+        provinsi: data.data.provinsi,
+        kota: data.data.kota,
+        alamat: data.data.alamat,
+        kontak: data.data.kontak,
+        updated_date: data.data.updated_date,
+        update: (
+          <a
+            onClick={() => {
+              handlerUpdate();
+            }}
+          >
+            <div className="btn btn-success">Update</div>
+          </a>
+        ),
+        delete: (
+          <a onClick={() => handlerDelete(data._id, data.namaToko)}>
+            <div className="btn btn-danger">Delete</div>
+          </a>
+        ),
+      };
+    }),
   };
 
   return (
     <>
-    <MDBDataTable responsive striped bordered small data={data} />;
+      <MDBDataTable responsive striped bordered small data={data} />
+      {showModal ? <Modal kondisi={true} /> : null}
     </>
-  )
-
+  );
 };
 
 export default Table;
---
+++ client/lib/deleteMitra.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+import Router from "next/router";
+export const deleteMitra = async (id) => {
+  const { data } = await axios.delete(
+    `http://localhost:3006/dataoksigen/${id}`
+  );
+  try {
+      if (data.status === 400) {
+        Router.reload();
+    }
+  } catch (error) {
+      alert(error);
+  }
+};
