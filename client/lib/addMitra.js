bccecbbcbb058b7affad5d99e3e424b9ab4b42e5
fandilladp <fandilladp@gmail.com>
2021-08-17 08:20:47 +0700

    set new id and new default input add data

--- client/lib/addMitra.js
+++ client/lib/addMitra.js
@@ -1,49 +1,48 @@
 import axios from "axios";
 import Router from "next/router";
-import { v4 as uuidv4 } from 'uuid';
-import { data } from "../data/dataProv";
+import { v4 as uuidv4 } from "uuid";
 export const addMitra = async (
   namaToko,
   username,
   provinsi,
   kota,
   alamat,
-  kontak,
+  kontak
 ) => {
   const idMitra = uuidv4();
-  const dataAuth = await axios.post("http://localhost:3006/auth/register",{
-    id_mitra: idMitra,  
+  const dataAuth = await axios.post("http://localhost:3006/auth/register", {
+    id_mitra: idMitra,
     username,
     password: "bantucarioksigen",
     role: "mitra",
-    verify: "belum terverivikasi"
-    });
-    const { data } = await axios.post("http://localhost:3006/dataoksigen", {
-      _id: idMitra,
-      namaToko,
-      status: "belum terverivikasi",
-      data: {
-        provinsi,
-        kota,
-        alamat,
-        kontak,
-        statusBuka: "Menuggu tehubung dengan mitra",
-        stokBarang: "Menuggu tehubung dengan mitra",
-        antrian: "Menuggu tehubung dengan mitra",
-        waktuBuka: "Menuggu tehubung dengan mitra",
-      },
-    });
-    try {
-        const validation = data.message.message;
-        if(!validation){
-           if(data.status === 200 && dataAuth.status){
-               Router.push('/adminOksigen');
-           }
-        }else{
-            alert(validation);
-        }
-    } catch (error) {
-       console.log(error)
+    verify: "belum terverivikasi",
+  });
+  const { data } = await axios.post("http://localhost:3006/dataoksigen", {
+    _id: idMitra,
+    namaToko,
+    status: "belum terverivikasi",
+    data: {
+      provinsi,
+      kota,
+      alamat,
+      kontak,
+      statusBuka: "Buka / Tutup",
+      stokBarang: "Stok Barang : .....",
+      antrian: "Antrian : ......",
+      waktuBuka: "00-00",
+      waktuTutup: "00-00",
+    },
+  });
+  try {
+    const validation = data.message.message;
+    if (!validation) {
+      if (data.status === 200 && dataAuth.status) {
+        Router.push("/adminOksigen");
+      }
+    } else {
+      alert(validation);
     }
-    
+  } catch (error) {
+    console.log(error);
+  }
 };
