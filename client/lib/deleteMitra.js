10e99c28ce15ac399a4aa82a58de73f347286223
fandilladp <fandilladp@gmail.com>
2021-08-17 08:21:18 +0700

    update route to succes delete

--- client/lib/deleteMitra.js
+++ client/lib/deleteMitra.js
@@ -4,9 +4,12 @@ export const deleteMitra = async (id) => {
   const { data } = await axios.delete(
     `http://localhost:3006/dataoksigen/${id}`
   );
+  const deleteMitra = await axios.delete(
+    `http://localhost:3006/auth/${id}`
+  );
   try {
       if (data.status === 200) {
-        Router.reload();
+        Router.push("/adminOksigen");
     }
   } catch (error) {
       alert(error);
