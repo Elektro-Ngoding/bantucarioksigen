94c49947271ca2b34bf7e1e12a3f7c0142f570c9
fandilladp <fandilladp@gmail.com>
2021-08-25 06:30:52 +0700

    add link active

--- component/Card/ShopCardAdmin.tsx
+++ component/Card/ShopCardAdmin.tsx
@@ -21,7 +21,7 @@ export default function ShopCardAdmin<T>(props: any) {
   return (
     <>
       <div className="bg-gray-50 flex flex-col rounded-md overflow-hidden max-w-xs hover:bg-gray-100 shadow-md ">
-        <Link href="tokopedia">
+        <Link href={props.product_url}>
           <a className="flex-shrink-0 hover:cursor-pointer">
             <div className="display:inline-block;max-width:100%;overflow:hidden;position:relative;box-sizing:border-box;margin:0">
               <Image
@@ -38,7 +38,7 @@ export default function ShopCardAdmin<T>(props: any) {
           <b>IDR: {props.product_price}</b>
           <p className="text-gray-900">{props.namaToko}</p>
           <p className="text-gray-500 text-xs sm:text-xs">
-            update : <Moment fromNow>{props.updated_date}</Moment>,
+            update : <Moment fromNow>{props.updated_date}</Moment>
           </p>
         </div>
         <div className="flex items-center align-middle justify-between">
