8ca4d8b1e3333374ebec283b8ed5931622f60706
fandilladp <fandilladp@gmail.com>
2021-08-25 06:32:08 +0700

    set filter and load state

--- pages/explore.tsx
+++ pages/explore.tsx
@@ -1,6 +1,7 @@
 import ShopCard from "../component/Card/ShopCard";
 import Layout from "../component/Layout/Layout";
-import Addres from "../component/Address/index";
+import Address from "../component/Address/index";
+import { useState, useEffect } from "react";
 
 interface Type {
   dataCard: Array<any>;
@@ -8,29 +9,76 @@ interface Type {
 
 export default function explore(props: Type) {
   const { dataCard } = props;
+  const [filteredData, setFilteredData] = useState(dataCard);
+  const [load, setLoad] = useState<boolean>(false);
+  useEffect(() => {
+    setFilteredData(dataCard);
+  }, []);
+
+  const handleLoad = () => {
+    setLoad(true);
+  };
+
+  const handleSearch = (data: string) => {
+    setLoad(false);
+    if (data !== "") {
+      const results = dataCard.filter((res) => {
+        return res.data.kota.toLowerCase().startsWith(data.toLowerCase());
+      });
+      setFilteredData(results);
+    } else {
+      setFilteredData([]);
+    }
+  };
 
   return (
     <Layout>
       <section className="overflow-hidden px-4 py-6 pb-4 space-y-4 pt-16">
         <div className="w-full sm:max-w-xl mx-auto pt-2 px-2">
           <div className="text-lg sm:text-xl mb-2">
-            <Addres />
+            <Address
+              handleLoad={() => {
+                handleLoad();
+              }}
+              handleSearch={(data: string) => handleSearch(data)}
+            />
             <hr />
           </div>
           <div className="grid grid-cols-3 gap-4">
-            {dataCard.map((res: any) => {
-              return (
-                <>
-                  <ShopCard
-                    image_url={res.data.image_url}
-                    namaBarang={res.data.namaBarang}
-                    product_price={res.data.product_price}
-                    namaToko={res.namaToko}
-                    product_url={res.data.product_url}
-                  />
-                </>
-              );
-            })}
+            {load ? (
+              <div className="px-4 overflow-hidden bg-white py-6 space-y-4">
+                <h1 className="animate-bounce text-center font-semibold text-gray-300 text-lg">
+                  Pilih Kab.kota kamu
+                </h1>
+              </div>
+            ) : (
+              <>
+                {filteredData.length === 0 ? (
+                  <div className="px-4 overflow-hidden bg-white py-6 space-y-4">
+                  <h1 className="animate-bounce text-center font-semibold text-gray-300 text-lg">
+                   Data not found ....
+                  </h1>
+                </div>
+                ) : (
+                  <>
+                    {filteredData.map((res: any) => {
+                      return (
+                        <>
+                          <ShopCard
+                            image_url={res.data.image_url}
+                            namaBarang={res.data.namaBarang}
+                            product_price={res.data.product_price}
+                            namaToko={res.namaToko}
+                            product_url={res.data.product_url}
+                            updated_date={res.data.updated_date}
+                          />
+                        </>
+                      );
+                    })}
+                  </>
+                )}
+              </>
+            )}
           </div>
         </div>
       </section>
