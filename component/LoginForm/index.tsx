0b86cea38294cae05267f66d70882d8e33311788
fandilladp <fandilladp@gmail.com>
2021-09-07 04:09:19 +0700

    new component login form

---
+++ component/LoginForm/index.tsx
@@ -0,0 +1,110 @@
+import React, { useState } from "react";
+import { loginAuth } from "../../lib/auth";
+
+const LoginForm = () => {
+  const [username, setUsername] = useState();
+  const [password, setPassword] = useState();
+  const [remember, setRemember] = useState(false);
+
+  const handleChangeUsername = (event: any) => {
+    setUsername(event.target.value);
+  };
+  const handleChangePassword = (event: any) => {
+    setPassword(event.target.value);
+  };
+  const handleCheckbox = () => {
+    {
+      remember ? setRemember(false) : setRemember(true);
+    }
+  };
+  const handleSubmit = (event: any) => {
+    event.preventDefault();
+    loginAuth(username, password, remember);
+  };
+  return (
+    <>
+      <div className="min-h-screen flex items-center justify-center bg-gray-100 py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in Relawan Mitra Oksigen
+            </h2>
+          </div>
+          <form onSubmit={handleSubmit} className="mt-8 space-y-6">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div>
+                <label htmlFor="Username" className="sr-only">
+                  Username
+                </label>
+                <input
+                  id="Username"
+                  name="username"
+                  type="username"
+                  autoComplete="username"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Username"
+                  onChange={(event) => handleChangeUsername(event)}
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                  onChange={(event) => handleChangePassword(event)}
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember"
+                  type="checkbox"
+                  className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded"
+                  onClick={handleCheckbox}
+                />
+                <label
+                  htmlFor="remember-me"
+                  className="ml-2 block text-sm text-gray-900"
+                >
+                  Remember me
+                </label>
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-900 hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500"
+              >
+                Sign in
+              </button>
+            </div>
+          </form>
+          <hr />
+          <div>
+            <button
+              type="button"
+              className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-green-600 hover:bg-green-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+              Daftar Menjadi Relawan Mitra Oksigen
+            </button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default LoginForm;
